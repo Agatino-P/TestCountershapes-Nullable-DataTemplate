8cba5e6515c5c4b8b5b536e957bc483159860c12
Agatino-P <57068089+Agatino-P@users.noreply.github.com>
2020-04-23 03:52:51 +0200

    Add project files.

---
+++ CounterShapes/CtrsContainer.cs
@@ -0,0 +1,115 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace CounterShapes
+{
+    public class Ctrs : ObservableObject
+    {
+        public bool Nullable { get; set; }
+
+        private double? _x;
+        public double? X { get => _x == null ? (Nullable?null:(double?)0) : _x; set => _x= value; }
+
+        private double? _y;
+        public double? Y { get => _y == null ? (Nullable ? null : (double?)0) : _y; set => _y = value; }
+
+        private double? _ym;
+        public double? Ym { get => _ym == null ? (Nullable ? null : (double?)0) : _ym; set => _ym = value; }
+
+        private double? _z;
+        public double? Z { get => _z == null ? (Nullable ? null : (double?)0) : _z; set => _z = value; }
+
+        private bool? _solidY;
+        public bool? SolidY { get => _solidY == null ? (Nullable ? null : (bool?)false) : _solidY; set => _solidY = value; }
+
+        private bool? _solidZ;
+        public bool? SolidZ { get => _solidZ == null ? (Nullable ? null : (bool?)false) : _solidZ; set => _solidZ = value; }
+
+        public Ctrs (bool nullable, double? x, double? y, double? ym, double? z, bool? solidY, bool? solidZ)
+        {
+            Nullable = nullable;
+             X = x;
+             Y = y;
+             Ym = ym;
+             Z = z;
+            SolidY = solidY;
+            SolidZ = solidZ;
+        }
+
+        public Ctrs()
+        {
+        }
+
+        public Ctrs ToValued()
+        {
+            return new Ctrs(false, X, Y, Ym, Z, SolidY, SolidZ);
+        }
+        public override string ToString()
+        {
+            return $"Nullable:{ Nullable} X:{ X} Y:{ Y} Ym:{ Ym} Z:{ Z} SolidY: {SolidY} SolidZ: {SolidZ}";
+        }
+
+        public Ctrs OverrideWith(Ctrs overridingCtrs)
+        {
+            Ctrs newCtrs = new Ctrs(Nullable, X, Y, Ym, Z, SolidY, SolidZ);
+
+            newCtrs.Nullable = (overridingCtrs.Nullable) ? Nullable : overridingCtrs.Nullable; //TBC
+            newCtrs.X = overridingCtrs.X == null ? X : overridingCtrs.X;
+            newCtrs.Y = overridingCtrs.Y == null ? Y : overridingCtrs.Y;
+            newCtrs.Ym = overridingCtrs.Ym == null ? Ym : overridingCtrs.Ym;
+            newCtrs.Z = overridingCtrs.Z == null ? Z : overridingCtrs.Z;
+            newCtrs.SolidY = overridingCtrs.SolidY == null ? SolidY : overridingCtrs.SolidY;
+            newCtrs.SolidZ = overridingCtrs.SolidZ == null ? SolidZ : overridingCtrs.SolidZ;
+
+            return newCtrs;
+        }
+
+        public static Ctrs GetEmpty()
+        { 
+            return new Ctrs(true, null, null, null, null, null, null); 
+        }
+
+    }
+
+    public class CtrsRecord : ObservableObject
+    {
+        private int? _phase; public int? Phase { get => _phase; set { Set(() => Phase, ref _phase, value); }}
+        private double _angle; public double Angle { get => _angle; set { Set(() => Angle, ref _angle, value); }}
+        private Ctrs _ctrs; public Ctrs Ctrs { get => _ctrs; set { Set(() => Ctrs, ref _ctrs, value); }
+}
+        
+        
+
+        public CtrsRecord(int? phase, double angle, Ctrs ctrs)
+        {
+            Phase = phase;
+            Angle = angle;
+            Ctrs = ctrs;
+        }
+
+        public CtrsRecord()
+        {
+            Ctrs = new Ctrs();
+        }
+    }
+
+    public class CtrsContainer :ObservableObject
+    {     
+        private BindingList<CtrsRecord> _records = new BindingList<CtrsRecord>();
+        public BindingList<CtrsRecord>  Records { get => _records; set { Set(() => Records, ref _records, value); }}
+        
+        public CtrsRecord GetRecordByPhase(int phase)
+        {
+            return Records.FirstOrDefault(cr => cr.Phase == phase);
+        }
+
+        public Ctrs GetCtrsByAngle(double angle)
+        {
+            return Records.FirstOrDefault(cr => cr.Angle == angle).Ctrs;
+        }
+    }
+}
---
+++ TestCountershapes/MainwindowViewModel.cs
@@ -0,0 +1,136 @@
+﻿using CounterShapes;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Windows;
+
+namespace TestCountershapes
+{
+    internal class MainwindowViewModel : ViewModelBase
+    {
+        //Col1
+        private CtrsContainer _ctrsRecords = new CtrsContainer();
+        public CtrsContainer CtrsRecords { get => _ctrsRecords; set => Set(() => CtrsRecords, ref _ctrsRecords, value); }
+
+        
+        private RelayCommand _addCmd;
+        public RelayCommand AddCmd => _addCmd ?? (_addCmd = new RelayCommand(
+            () => add(),
+            () => true
+            ));
+
+        private void add()
+        {
+            CtrsRecords.Records.Add(CtrsRecordAdd);
+        }
+
+        private CtrsRecord _ctrsRecordAdd = new CtrsRecord();
+        public CtrsRecord CtrsRecordAdd { get => _ctrsRecordAdd; set => Set(() => CtrsRecordAdd, ref _ctrsRecordAdd, value); }
+
+        //Col2
+
+        private int _phase; public int Phase { get => _phase; set => Set(() => Phase, ref _phase, value); }
+
+        private RelayCommand _searchPhaseCmd;
+        public RelayCommand SearchPhaseCmd => _searchPhaseCmd ?? (_searchPhaseCmd = new RelayCommand(
+            () => searchPhase(),
+            () => true
+            ));
+
+        private void searchPhase()
+        {
+            CtrsRecord ctrsRecord = CtrsRecords.GetRecordByPhase(Phase);
+            if (ctrsRecord == null)
+            {
+                MessageBox.Show($"Phase: {Phase} not found", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                CtrsFound = new Ctrs();
+            }
+            else
+            {
+                CtrsFound = ctrsRecord.Ctrs;
+            }
+        }
+
+        private double _angle; public double Angle { get => _angle; set => Set(() => Angle, ref _angle, value); }
+
+        private RelayCommand _searchAngleCmd;
+        public RelayCommand SearchAngleCmd => _searchAngleCmd ?? (_searchAngleCmd = new RelayCommand(
+            () => searchAngle(),
+            () => true
+            ));
+
+        private void searchAngle()
+        {
+            Ctrs ctrs = CtrsRecords.GetCtrsByAngle(Angle);
+            if (ctrs == null)
+            {
+                MessageBox.Show($"Angle: {Angle} not found", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                CtrsFound = new Ctrs();
+            }
+            else
+            {
+                CtrsFound = ctrs;
+            }
+        }
+
+        private Ctrs _ctrsFound = new Ctrs();
+        public Ctrs CtrsFound { get => _ctrsFound; set => Set(() => CtrsFound, ref _ctrsFound, value); }
+
+        private Ctrs _ctrsNullable= new Ctrs();
+        public Ctrs CtrsNullable { get => _ctrsNullable; set { Set(() => CtrsNullable, ref _ctrsNullable, value); }}
+
+
+        private RelayCommand _toValuedCmd;
+        public RelayCommand ToValuedCmd => _toValuedCmd ?? (_toValuedCmd = new RelayCommand(
+            () =>  toValuedCmd(),
+            () => true
+            ));
+
+        private void toValuedCmd()
+        {
+            CtrsValued = CtrsNullable.ToValued();
+        }
+
+        private Ctrs _ctrsValued = new Ctrs();
+        public Ctrs CtrsValued { get => _ctrsValued; set { Set(() => CtrsValued, ref _ctrsValued, value); }}
+
+        //Col3
+
+        private Ctrs _ctrsOverlapped = new Ctrs();
+        public Ctrs CtrsOverlapped { get => _ctrsOverlapped; set => Set(() => CtrsOverlapped, ref _ctrsOverlapped, value); }
+
+        private Ctrs _ctrsOverlapping = new Ctrs();
+        public Ctrs CtrsOverlapping { get => _ctrsOverlapping; set => Set(() => CtrsOverlapping, ref _ctrsOverlapping, value); }
+
+        private RelayCommand _overlapCmd;
+        public RelayCommand OverlapCmd => _overlapCmd ?? (_overlapCmd = new RelayCommand(
+            () => overlap(),
+            () => { return 1 == 1; }
+            ));
+
+        private void overlap()
+        {
+            CtrsResult=CtrsOverlapped.OverrideWith(CtrsOverlapping);
+        }
+
+        private Ctrs _ctrsResult;
+        public Ctrs CtrsResult { get => _ctrsResult; set => Set(() => CtrsResult, ref _ctrsResult, value); }
+
+
+        public MainwindowViewModel()
+        {
+            loadSampleData();
+        }
+
+
+
+
+
+
+        private void loadSampleData()
+        {
+            CtrsRecords.Records.Add(new CtrsRecord(1, 0, new Ctrs(false, 101, 102, 103, 104, true, true)));
+            CtrsRecords.Records.Add(new CtrsRecord(null, 90, new Ctrs(false, 201, 202, 203, 204, true, true)));
+        }
+    }
+}
